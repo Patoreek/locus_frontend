9071fb31a4cbc84d5e9e400ac45f631734695092
Patrick Minda <patrickminda@hotmail.com>
2020-05-15 22:37:00 +1000

    Added sites to guestpanel. now users can see containers of each divesite

--- src/containers/InformationPanel/GuestPanel/GuestPanel.js
+++ src/containers/InformationPanel/GuestPanel/GuestPanel.js
@@ -1,11 +1,38 @@
-import React from 'react';
+import React, { useEffect, useContext} from 'react';
+
+import { DiveSitesContext } from '../../../context/DiveSiteContext';
+
+import StarRating from '../../../components/StarRating/StarRating';
+
+import classes from './GuestPanel.module.css';
 
 const GuestPanel = () => {
+
+    const [diveSites, setDiveSites] = useContext(DiveSitesContext);
+
+    //console.log(diveSites);
+
+
     return (
         <div>
             <h1>GUEST PANEL</h1>
+        {diveSites === [] && (
+            <div>
             <p> Users that dont have an account but want to search sites view</p>
             <p> What the user will see </p>
+            </div>
+        )}
+        {diveSites.map(site => (
+            <div className={classes.siteContainer}>
+            <b>{site.name}, {site.area}</b>
+            <p> {site.description} </p>
+            <img src={'http://localhost:8080/' + site.images[0]}
+                 className={classes.siteImage}
+            />
+            <StarRating/>
+            </div>
+        ))}
+
         </div>
     );
 };
