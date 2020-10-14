c0397efe8f44ef35bca53e4d8fbce5b34cc8063a
Patrick <patrick.minda@hotmail.com>
2020-10-14 21:29:20 +1100

    Core styling of profile page completed.

--- src/containers/ProfileView/ProfileView.js
+++ src/containers/ProfileView/ProfileView.js
@@ -3,17 +3,23 @@ import { useHistory } from 'react-router-dom';
 import { AuthContext, LoadingContext } from '../../context/AuthContext';
 import { SiteContext, DetailsContext } from '../../context/DiveSiteContext';
 
+import { ReactComponent as PinSVG } from '../../assets/icons/pin.svg';
+import { ReactComponent as XpSVG } from '../../assets/icons/experience.svg';
+
 import Details from '../DetailsView/DetailsView';
 
 import Spinner from 'react-bootstrap/Spinner';
 
-import classes from './ProfileView.module.css';
+import StarRating from '../../components/StarRating/StarRating';
+import FavouriteButton from '../../components/Buttons/FavouriteButton/FavouriteButton';
+
+import classes from './ProfileView.module.scss';
 
 const ProfileView = () => {
 
     const [isAuth, setIsAuth] = useContext(AuthContext);
     const [ selectedSite, setSelectedSite ] = useContext(SiteContext);
-    const [moreDetails, setMoreDetails] = useContext(DetailsContext);
+    //const [moreDetails, setMoreDetails] = useContext(DetailsContext);
 
 
     const [isLoading, setIsLoading] = useState(true);
@@ -33,12 +39,12 @@ const ProfileView = () => {
             history.replace('/login');
         }
 
-        const siteLinkHandler = (site) => {
-            console.log('Site Name Pressed!');
-            console.log(site);
-            setSelectedSite(site);
-            setMoreDetails(!moreDetails);
-        }
+        // const siteLinkHandler = (site) => {
+        //     console.log('Site Name Pressed!');
+        //     console.log(site);
+        //     setSelectedSite(site);
+        //     setMoreDetails(!moreDetails);
+        // }
     
         useEffect(() => {
             async function getProfile() {
@@ -69,98 +75,90 @@ const ProfileView = () => {
 
 
     return (
-        <div className={classes.profilePage}>
-        {!moreDetails && (
-        <div className={classes.profilePageContainer}>
+        <div>
             {!isLoading && (
-                <div>
-                    <div className={classes.coverPhotoContainer}>
-                       
-                    </div>
-
-                    <div className={classes.topHalfContainer}>
-
-                         <div className={classes.profilePictureContainer}>
-                            <div className={classes.overlayDiv}></div>
-                            <img src={profilePic} className={classes.profilePicture}/> 
-                        </div>
-
-                        <div className={classes.profileNameContainer}>
-                            <h3 className={classes.profileName}>{firstName} {lastName}</h3>
-                        </div>
-
-                        <div className={classes.profileLocationContainer}>
-                            <p className={classes.profileLocation}>{location}</p>
-                        </div>
-                        <div className={classes.profileSpanContainer}>
-                            <p className={classes.profileSpan}>|</p>
-                        </div>
-
-                        <div className={classes.profileLicenseContainer}>
-                            <p className={classes.profileLicense}>{licenseType}</p>
-                        </div>
-
-                        <div className={classes.profileEditContainer}>
-                            <a href="/editprofile" className={classes.profileEdit}>Edit profile</a>
-                        </div>
-                    </div>
-
-
-                   
-                
-
-                        <div className={classes.profileBioContainer}>
-                            <div className={classes.profileBioHeaderContainer}>
-                                <h3 className={classes.profileBioHeader}>Bio</h3>
+                <div className={classes.profilePage}>
+                    <div className={classes.profile}>
+                        <div className={classes.profileLeft}>
+                            <div className={classes.profileLeft__pictureContainer}>
+                                <img src={profilePic} className={classes.picture}/> 
                             </div>
-                            <p className={classes.profileBio}>{bio}</p>
                         </div>
 
+                        <div className={classes.profileRight}>
+                            <div className={classes.profileRight__nameContainer}>
+                                <h3 className={classes.name}>{firstName} {lastName}</h3>
+                            </div>
+
+                            <div className={classes.profileRight__bioContainer}>   
+                                <p className={classes.bio}>{bio}</p>
+                            </div>
+
+                            <div className={classes.profileRight__locationContainer}>
+                                <PinSVG className={classes.icon}/>
+                                <p className={classes.location}>{location}</p> {/* Change this to City, Country */}
+                            </div>
+
+                            <div className={classes.profileRight__experienceContainer}>
+                                <XpSVG className={classes.icon}/>
+                                <p className={classes.experience}>{licenseType}</p>
+                            </div>
+
+                            <div className={classes.profileRight__editContainer}>
+                                <a href="/editprofile" className={classes.edit}>Edit profile</a>
+                            </div>
+                        </div>
+
+                        
+                    </div>
+
+                    <div className={classes.reportsContainer}>
+                        <h3>Dive Reports (999+)</h3>
+                        <div className={classes.report}>
+                            <h3> Under Construction</h3>
+                        </div>
+                    </div>
                     
-                        <div className={classes.profileFavContainer}>
+                    <div className={classes.favouritesContainer}>
+                        <h3 className={classes.header}>{firstName}'s Favourites</h3>
+                        <div className={classes.favourites}>
+                            {favourites.map(favourite => (
+                                <div className={classes.favouriteContainer}>
 
-                            <div className={classes.profileFavHeaderContainer}>
-                                <h3 className={classes.profileFavHeader}>{firstName}'s Favourites</h3>
-                            </div>
-            
-                            <div className={classes.favouritesContainer}>
-                                {favourites.map(favourite => (
-                                    <div className={classes.favouriteContainer}>
+                                    <div className={classes.favouriteContainer__imageContainer}>
 
-                                        <div className={classes.siteHeaderContainer}>
-                                            <h3 className={classes.siteHeader}
-                                                onClick={() => siteLinkHandler(favourite.site)}>
-                                
-                                                {favourite.siteName}, {favourite.siteArea}
-                                            
-                                            </h3>
-                                        </div>
-
-                                        <div className={classes.siteImageContainer}>
-
-                                            <img src={'http://localhost:8080/' + favourite.siteImage} 
-                                                className={classes.siteImage}
-                                            />
-                                        </div>
-
-
-
+                                        <img src={'http://localhost:8080/' + favourite.siteImage} 
+                                        className={classes.image}
+                                        />
                                     </div>
-                                ))}
-                            </div>
-    
-                        </div> 
+                                    <div className={classes.favouriteContainer__pointContainer}>
+                                        <span className={classes.point}>Shore Dive · Great for Scuba</span>
+                                    </div>
+                                    <div className={classes.favouriteContainer__nameContainer}>
+                                        <span className={classes.name}>{favourite.siteName}, {favourite.siteArea}</span>
+                                    </div>
+                                    
+                                    <div className={classes.favouriteContainer__descriptionContainer}>
+                                        <span className={classes.description}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</span>
+                                    </div>
+                                    <div className={classes.favouriteContainer__ratingContainer}>
+                                        <div className={classes.rating}>
+                                            <StarRating/>
+                                        </div>
+                                    </div>
+                                    <div className={classes.favouriteContainer__moreContainer}>
+                                        <a href="#" className={classes.more}>More...</a>
+                                    </div>
+                                </div>
+                            ))}
+                        </div>
+
+                        
+                    </div> 
 
                 </div>
             )}
-            {isLoading && (
-                <Spinner animation="border" />
-            )}
-        </div>
-        )}
-        {moreDetails && (
-        <Details/>
-        )}
+            
         </div>
 
             
