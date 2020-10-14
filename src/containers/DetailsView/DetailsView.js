c0571ed27fab991b02a830eede1ee03596ff2cd3
Patrick <patrick.minda@hotmail.com>
2020-10-14 17:28:28 +1100

    Core of Dive site page complete.

--- src/containers/DetailsView/DetailsView.js
+++ src/containers/DetailsView/DetailsView.js
@@ -17,6 +17,15 @@ import FavouriteButton from '../../components/Buttons/FavouriteButton/FavouriteB
 import shoreIcon from '../../images/locationIcons/ShoreIcon.svg';
 import boatIcon from '../../images/locationIcons/BoatIcon.svg';
 
+//TODO: V1.1 SVG Imports
+
+import { ReactComponent as ShoreSVG } from '../../assets/icons/shore.svg';
+import { ReactComponent as ReefSVG } from '../../assets/icons/reef.svg';
+import { ReactComponent as DepthSVG } from '../../assets/icons/depth.svg';
+import { ReactComponent as XpSVG } from '../../assets/icons/experience.svg';
+import { ReactComponent as TemperatureSVG } from '../../assets/icons/temperature.svg';
+import { ReactComponent as VisibilitySVG } from '../../assets/icons/visibility.svg';
+
 
 
 import classes from './DetailsView.module.scss';
@@ -25,7 +34,7 @@ const Details = (props) => {
     const siteId = props.match.params.id;
     console.log(siteId);
     
-    //const [selectedSite, setSelectedSite] = useContext(SiteContext);
+    const [selectedSite, setSelectedSite] = useContext(SiteContext);
     const [moreDetails, setMoreDetails] = useContext(DetailsContext);
     const [isAuth, setIsAuth] = useContext(AuthContext);
 
@@ -114,6 +123,7 @@ const Details = (props) => {
             setSiteVisibility(site.visibility);
             setSiteUnderwaterMap(site.underwaterMap);
             setSiteFeatures(site.commonFeatures);
+            setSelectedSite(site);
 
             setIsLoading(false);
     
@@ -130,34 +140,111 @@ const Details = (props) => {
     // // console.log('WEATHERCONTENT');
     // // console.log(weatherContent);
     // //console.log(weatherContent);
+    console.log(siteImages);
 
     return (
-      <div className={classes.diveSite}>
-         <h3> Testing Header Tag</h3>
+      <div>
           {!isLoading && (
-            <div>
-                <h3> isLoading is now turned OFF! Should be working...</h3>
+            <div className={classes.divesite}>
+                {/* <h3> isLoading is now turned OFF! Should be working...</h3> */}
 
-
-                <h3>Shellharbour, New South Wales, Australia</h3> {/* Should be a link to the map / google maps of the area?? */}
-                <h3>{siteName}, {siteArea}</h3>
-                <h3>Shellharbour, New South Wales, Australia</h3> {/* Should be a link to the map / google maps of the area?? */}
-                <h3>See all community photos</h3>
-                <h3> List of info with icons</h3>
-                <h3>Access: {siteType}</h3>
-                <h3>Depth: {siteDepth}m</h3>
-                <h3>Visibility: {siteVisibility}m</h3>
-                <h3>Experience Level: Open Water ...</h3>
-                <h3>Average Temperature: 18deg </h3>
-                <h3>Dive Environment: Reef Dive / Wreck Dive </h3>
-
-                <h3>{siteDescription}</h3>
-                <div>
-                  Weather Container (PLACE THIS AND IMPLEMENT LATER)
+                <div className={classes.divesite__locationTop}>
+                  <a href="#">Shellharbour, New South Wales, Australia</a> {/* Should be a link to the map / google maps of the area?? */}
+                </div>
+                
+                <div className={classes.divesite__imageGridContainer}>
+                  <div className={`${classes.image} ${classes.image__1}`}>
+                    <img className={classes.image} src={'http://localhost:8080/' + siteImages[0]}/>
+                  </div>
+                  <div className={`${classes.image} ${classes.image__2}`}>
+                    <img className={classes.image} src={'http://localhost:8080/' + siteImages[1]}/>
+                  </div>
+                  <div className={`${classes.image} ${classes.image__3}`}>
+                    <img className={classes.image} src={'http://localhost:8080/' + siteImages[2]}/>
+                  </div>
+                  <div className={`${classes.image} ${classes.image__4}`}>
+                    <img className={classes.image} src={'http://localhost:8080/' + siteImages[0]}/>
+                  </div>
+                  <div className={`${classes.image} ${classes.image__5}`}>
+                    <img className={classes.image} src={'http://localhost:8080/' + siteImages[1]}/>
+                  </div>
                 </div>
 
-                <div>
-                  <h3>DIVE REPORTS (UNDER CONSTRUCTION)</h3>
+                {/* USE FLEXBOX COLUMN */}
+                <div className={classes.divesite__nameContainer}>
+                    <h3 className={classes.name}>{siteName}, {siteArea}</h3>
+                    <a href="#">Shellharbour, New South Wales, Australia</a> {/* Should be a link to the map / google maps of the area?? */}
+                    <div className={classes.ratingsContainer}>
+                      <div className={classes.ratingsContainer__rating}>
+                      <StarRating site={selectedSite}/>
+                      </div>
+                      <div className={classes.ratingsContainer__favBtn}>
+                        <FavouriteButton site={selectedSite}/>
+                      </div>
+                    </div>
+                </div>
+
+
+                <div className={classes.divesite__points}>
+                    <div className={classes.point}> 
+                      <ShoreSVG className={classes.point__icon}/> 
+                      <div className={classes.point__text}>                         
+                        <span>{siteType == 1 ? "Shore Dive" : "Boat Dive"}</span>
+                      </div>
+                    </div>
+                    <div className={classes.point}>
+                      <ReefSVG className={classes.point__icon}/>
+                      <div className={classes.point__text}>            
+                        <span>Reef Dive</span>
+                      </div>
+                    </div>
+                    <div className={classes.point}>
+                      <DepthSVG className={classes.point__icon}/>   
+                      <div className={classes.point__text}>         
+                        <span>Max Depth: {siteDepth}m</span>
+                        <span>Average Depth: {siteDepth}m</span>
+                      </div>
+                    </div>
+                    <div className={classes.point}>
+                      <XpSVG className={classes.point__icon}/>  
+                      <div className={classes.point__text}>          
+                        <span>Open Water Scuba Diver</span>
+                        <span>Great for Snorkelling</span>
+                      </div>
+                    </div>
+                    <div className={classes.point}>
+                      <TemperatureSVG className={classes.point__icon}/>  
+                      <div className={classes.point__text}>
+                        <span>18°C - 24°C</span>   
+                      </div>       
+                    </div>
+                    <div className={classes.point}>
+                      <VisibilitySVG className={classes.point__icon}/> 
+                      <div className={classes.point__text}>           
+                        <span>7 - {siteVisibility}m</span>
+                      </div>
+                    </div>
+                  
+                </div>
+
+                <div className={classes.divesite__descriptionContainer}>
+                  <p>{siteDescription}</p>
+                </div>
+
+                <div className={classes.divesite__rightContainer}>
+                  <a href="#" className={classes.photoLink}>See all community photos</a>
+                  <div className={classes.weatherContainer}>
+                    Weather Container (PLACE THIS AND IMPLEMENT LATER)
+                  </div>
+                </div>
+
+          
+                {/* //! DIVE REPORTS SECTION WILL GO HERE  */}
+                <div className={classes.divesite__reportsContainer}>
+                  <h4>Dive Reports (999+)</h4>
+                  <div className={classes.underConstruction}>
+                    <span> (UNDER CONSTRUCTION) </span> 
+                  </div>
                 </div>
              
 
